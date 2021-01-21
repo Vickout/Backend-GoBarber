4270b0e93150a1aef0799d294a5ddf2f41792b8f
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-21 19:22:14 -0300

    Altered type id column from varchar to uuid

--- src/database/migrations/1611165614061-CreateAppointments.ts
+++ src/database/migrations/1611165614061-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1611165614061
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
