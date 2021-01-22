15e316dc12f7fac0a9b67d03745e03cfae55db93
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-22 12:53:08 -0300

    Created authenticate session route

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async excute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, '8ee4d7b50a15e7544399022eb259ae9e', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
