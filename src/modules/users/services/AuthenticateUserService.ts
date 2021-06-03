db3c837343910933b4ce72e21ebb73aba048c283
Victor <vlourenco.m@gmail.com>
2021-06-03 18:16:30 -0300

    refactoring user module

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
@@ -6,22 +5,23 @@ import authConfig from '@config/auth';
 
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   email: string;
   password: string;
 }
 
-interface Response {
+interface IResponse {
   userData: User;
   token: string;
 }
 
 class AuthenticateUserService {
-  public async excute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const userData = await usersRepository.findOne({ where: { email } });
+  public async excute({ email, password }: IRequest): Promise<IResponse> {
+    const userData = await this.usersRepository.findByEmail(email);
 
     if (!userData) {
       throw new AppError('Incorrect email/password combination.', 401);
