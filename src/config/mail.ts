24f1c3098d0db5bb1b121877b18c7f10ca0faaae
Victor <vlourenco.m@gmail.com>
2022-03-01 12:06:17 -0300

    feat: create SES mail provider

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: '', // emails cadastrados no SES
+      name: '', // nome do usuário
+    },
+  },
+} as IMailConfig;
