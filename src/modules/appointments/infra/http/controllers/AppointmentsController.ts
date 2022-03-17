44469951f7c1eb122ea77ffbc88079c827b74b2e
Victor <vlourenco.m@gmail.com>
2022-03-16 21:01:19 -0300

    refactor: some changes

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,23 +1,30 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import Appointment from '../../typeorm/entities/Appointment';
 
 export default class AppointmentsController {
-  public async create(request: Request, response: Response): Promise<Response> {
+  public async create(
+    request: Request,
+    response: Response,
+  ): Promise<Response | undefined> {
     const { provider_id, date } = request.body;
     const user_id = request.user.id;
+    let appointment = {} as Appointment;
+    try {
+      const createAppointment = container.resolve(CreateAppointmentService);
 
-    const parsedDate = parseISO(date);
+      appointment = await createAppointment.execute({
+        date,
+        user_id,
+        provider_id,
+      });
 
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      user_id,
-      provider_id,
-    });
+      // return response.json(appointment);
+    } catch (error) {
+      console.log(error);
+    }
 
     return response.json(appointment);
   }
